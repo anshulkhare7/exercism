fbead366df86aa9f07b5645c3830be0a3b8ee200
Anshul <anshulkhare7@gmail.com>
2024-11-14 21:23:45 +0530

    java and sqlite

---
+++ sqlite/darts/darts.sql
@@ -0,0 +1,15 @@
+-- Schema: CREATE TABLE "darts" ("x" REAL, "y" REAL, score INTEGER);
+-- Task: update the darts table and set the score based on the x and y values.
+Insert into DARTS values (-9, 9, 0);
+Insert into DARTS values (0, 10, 1);
+Insert into DARTS values (-5, 0, 5);
+Insert into DARTS values (0, -1, 10);
+Insert into DARTS values (0, 0, 10);
+Insert into DARTS values (-0.1, -0.1, 10);
+Insert into DARTS values (0.7, 0.7, 10);
+Insert into DARTS values (0.8, -0.8, 5);
+Insert into DARTS values (-3.5, 3.5, 5);
+Insert into DARTS values (-3.6, -3.6, 1);
+Insert into DARTS values (-7.0, 7.0, 1);
+Insert into DARTS values (7.1, -7.1, 0);
+Insert into DARTS values (0.5, -4, 5);
